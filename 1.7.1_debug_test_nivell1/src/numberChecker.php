594b7d9027616fafd3a3e34a57dc2fad11794221
miguel garcia <miguel.gareva@gmail.com>
2024-12-16 13:33:13 +0100

    NumberChecker tests created

--- 1.7.1_debug_test_nivell1/src/numberChecker.php
+++ 1.7.1_debug_test_nivell1/src/numberChecker.php
@@ -1,12 +1,17 @@
 <?php
 namespace Miguel\Ex1Lvl1;
+
 class NumberChecker  {
+
+
 	public function __construct(private int $number){}
+
 	public function isEven(): bool {
 		return $this->number%2 == 0;
 	}
 	public function isPositive(): bool {
 		return $this->number > 0;
 	}
+
 }
 ?>
