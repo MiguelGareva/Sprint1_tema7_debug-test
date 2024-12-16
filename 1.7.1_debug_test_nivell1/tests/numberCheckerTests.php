7f83660f3928d1856430e0eba3c4d671e6cccd29
miguel garcia <miguel.gareva@gmail.com>
2024-12-16 11:44:38 +0100

    numberCheckerTest class and his methods created

---
+++ 1.7.1_debug_test_nivell1/tests/numberCheckerTests.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Miguel\tests;
+
+use PHPUnit\Framework\TestCase;
+use Miguel\Ex1Lvl1\NumberChecker;
+
+class NumberCheckerTests extends NumberChecker{
+
+    public function testIsEven(): void{
+
+    }
+}
+
+?>
