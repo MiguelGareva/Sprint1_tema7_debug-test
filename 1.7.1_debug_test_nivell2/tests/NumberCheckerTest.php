bb017e3ee70b394fe083cdb52541e1bc1b7f7d8d
miguel garcia <miguel.gareva@gmail.com>
2025-03-27 11:39:37 +0100

    composer.json and readme updated, data providers implemented

--- 1.7.1_debug_test_nivell2/tests/NumberCheckerTest.php
+++ 1.7.1_debug_test_nivell2/tests/NumberCheckerTest.php
@@ -7,6 +7,34 @@ use Miguel\Ex1Lvl1\NumberChecker;
 
 class NumberCheckerTest extends TestCase{
 
+    public static function evenNumberProvider(): array{
+        return [
+            'cero es par' => [0],
+            'numero par positivo' => [2],
+            'numero par negativo' => [-4]
+        ];
+    }
+
+    public static function oddNumberProvider(): array{
+        return [
+            'numero impar positivo' => [3],
+            'numero impar negativo' => [-5]
+        ];
+    }
+
+    public static function positiveNumberProvider(): array{
+        return [
+            'numero par positivo' => [4],
+            'numero impar positivo' => [7]
+        ];
+    }
+
+    public static function negativeNumberProvider(): array{
+        return [
+            'numero par negativo' => [-6],
+            'numero impar negativo' => [-9]
+        ];
+    }
     public function testIsEven(){
         $numberChecker1 = new NumberChecker(3);
 
